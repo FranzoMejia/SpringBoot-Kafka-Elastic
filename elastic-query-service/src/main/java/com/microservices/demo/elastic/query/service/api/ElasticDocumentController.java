9de675eb990c91c43531dfa1ade52006d2a57ffc
Oscar Mejia <camecac35@gmail.com>
2026-01-29 10:29:23 -0600

    Addin URI versioning

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
@@ -3,6 +3,7 @@ package com.microservices.demo.elastic.query.service.api;
 import com.microservices.demo.elastic.query.service.business.ElasticQueryService;
 import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceRequestModel;
 import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceResponseModel;
+import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceResponseModelv2;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import org.slf4j.Logger;
@@ -24,14 +25,14 @@ public class ElasticDocumentController {
     }
 
 
-    @GetMapping("/")
+    @GetMapping("/v1")
     public @ResponseBody ResponseEntity<List<ElasticQueryServiceResponseModel>> getAllDocuments() {
         List<ElasticQueryServiceResponseModel> documents = elasticQueryService.getAllDocuments();
         LOG.info("Retrieving all documents. Total documents found: {}", documents.size());
         return ResponseEntity.ok(documents);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("v1/{id}")
     public @ResponseBody ResponseEntity<ElasticQueryServiceResponseModel> getDocumentById(@PathVariable @NotEmpty String id) {
         LOG.info("Retrieving document with id: {}", id);
         ElasticQueryServiceResponseModel document = elasticQueryService.getDocumentById(id);
@@ -42,7 +43,30 @@ public class ElasticDocumentController {
         return ResponseEntity.ok(document);
     }
 
-    @PostMapping("/get-document-by-text")
+    @GetMapping("v2/{id}")
+    public @ResponseBody ResponseEntity<ElasticQueryServiceResponseModelv2> getDocumentByIdv2(@PathVariable @NotEmpty String id) {
+        LOG.info("Retrieving document with id: {}", id);
+        ElasticQueryServiceResponseModel document = elasticQueryService.getDocumentById(id);
+        if (document == null) {
+            LOG.warn("Document with id: {} not found", id);
+            return ResponseEntity.notFound().build();
+        }
+        ElasticQueryServiceResponseModelv2 documentv2 = getV2Model(document);
+        return ResponseEntity.ok(documentv2);
+    }
+
+    private ElasticQueryServiceResponseModelv2 getV2Model(ElasticQueryServiceResponseModel document) {
+        ElasticQueryServiceResponseModelv2 responseModelv2 = ElasticQueryServiceResponseModelv2.builder()
+                .id(Long.parseLong(document.getId()))
+                .text(document.getText())
+                .userId(document.getUserId())
+                .createdAt(document.getCreatedAt())
+                .build();
+        responseModelv2.add(document.getLinks());
+        return responseModelv2;
+    }
+
+    @PostMapping("v1/get-document-by-text")
     public @ResponseBody ResponseEntity<List<ElasticQueryServiceResponseModel>> getDocumentByText(@RequestBody @Valid ElasticQueryServiceRequestModel elasticQueryServiceRequestModel) {
         List<ElasticQueryServiceResponseModel> documents = elasticQueryService.getDocumentsByText(elasticQueryServiceRequestModel.getText());
         LOG.info("Retrieving documents with text: {}. Total documents found: {}", elasticQueryServiceRequestModel.getText(), documents.size());
